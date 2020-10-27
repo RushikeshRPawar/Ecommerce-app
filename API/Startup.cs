82f80406ae006e4a2150d21796a720fd62b19c1f
Pawar <rushikesh.ramesh.pawar@hp.com>
2020-10-27 23:31:42 +0530

    End of section 13

--- API/Startup.cs
+++ API/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using API.Extensions;
+using StackExchange.Redis;
 
 namespace API
 {
@@ -30,7 +31,14 @@ namespace API
                 {
                     x.UseSqlite(_config.GetConnectionString("DefaultConnection"));
                 }
-            );     
+            ); 
+
+            services.AddSingleton<IConnectionMultiplexer>(
+                c => {
+                    var configuration = ConfigurationOptions.Parse(_config.GetConnectionString("Redis"),true);
+                    return ConnectionMultiplexer.Connect(configuration);
+                }
+            );    
             services.AddApplicationServices();       
 
             services.AddSwaggerDocumentation();
